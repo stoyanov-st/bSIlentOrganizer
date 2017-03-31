66861d100fedc5337f4b3112a84cf5035df2386f
stoyanov-st <st.stoyanov04@gmail.com>
2017-03-31 11:52:33 +0300

    RegisterOrLoginActivity

--- app/src/main/java/bg/uni_ruse/stoyanov/bsilentorganizer/MainActivity.java
+++ app/src/main/java/bg/uni_ruse/stoyanov/bsilentorganizer/MainActivity.java
@@ -1,7 +1,9 @@
 package bg.uni_ruse.stoyanov.bsilentorganizer;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,5 +13,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+    }
+
+    public void loginWithOrRegister(View view) {
+        Intent intent = new Intent(this, loginWithOrRegister.class);
+        startActivity(intent);
+                
     }
 }
---
+++ app/src/main/java/bg/uni_ruse/stoyanov/bsilentorganizer/loginWithOrRegister.java
@@ -0,0 +1,13 @@
+package bg.uni_ruse.stoyanov.bsilentorganizer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class loginWithOrRegister extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_with_or_register);
+    }
+}
