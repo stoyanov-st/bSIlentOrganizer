cb1694b7a2b01531dc93213bfb08cff9b5694152
stoyanov-st <st.stoyanov04@gmail.com>
2017-05-16 16:26:26 +0300

    w410dp values added removed focus and soft keyboard when tapped outside of editbox

--- app/src/main/java/bg/uni_ruse/stoyanov/bsilentorganizer/MainActivity.java
+++ app/src/main/java/bg/uni_ruse/stoyanov/bsilentorganizer/MainActivity.java
@@ -1,9 +1,12 @@
 package bg.uni_ruse.stoyanov.bsilentorganizer;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,5 +21,15 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, loginWithOrRegister.class);
         startActivity(intent);
                 
+    }
+
+
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+        if (getCurrentFocus() != null) {
+            InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
+        }
+        return super.dispatchTouchEvent(ev);
     }
 }
