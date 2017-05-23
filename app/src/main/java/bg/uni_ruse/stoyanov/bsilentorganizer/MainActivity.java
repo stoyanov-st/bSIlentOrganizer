910f8f9e1b4e32ee3d4c724a26e308e3cae15b2b
stoyanov-st <st.stoyanov04@gmail.com>
2017-05-23 16:45:38 +0300

    w410dp values added removed focus and soft keyboard when tapped outside of editbox

--- app/src/main/java/bg/uni_ruse/stoyanov/bsilentorganizer/MainActivity.java
+++ app/src/main/java/bg/uni_ruse/stoyanov/bsilentorganizer/MainActivity.java
@@ -8,22 +8,47 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
+
 public class MainActivity extends AppCompatActivity {
+
+    private GoogleApiClient mGoogleApiClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Configure sign-in to request the user's ID, email address, and basic
+        // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+        // Build a GoogleApiClient with access to the Google Sign-In API and the
+        // options specified by gso.
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this /* FragmentActivity */, this /* OnConnectionFailedListener */)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+
+        // Set the dimensions of the sign-in button.
+        SignInButton signInButton = (SignInButton) findViewById(R.id.sign_in_button);
+        signInButton.setSize(SignInButton.SIZE_WIDE);
+        signInButton.setColorScheme(SignInButton.COLOR_AUTO);
     }
 
+/*  Start login activity
     public void loginWithOrRegister(View view) {
         Intent intent = new Intent(this, loginWithOrRegister.class);
         startActivity(intent);
                 
-    }
+    }*/
 
-
+    //Hide soft keyboard when tapped outside of editViеws
     @Override
     public boolean dispatchTouchEvent(MotionEvent ev) {
         if (getCurrentFocus() != null) {
@@ -32,4 +57,6 @@ public class MainActivity extends AppCompatActivity {
         }
         return super.dispatchTouchEvent(ev);
     }
+
+
 }
