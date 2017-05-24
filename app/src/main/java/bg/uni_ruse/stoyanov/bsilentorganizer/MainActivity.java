1241d362b44a6a3f94cdb9403b6a4a63f9e705a4
stoyanov-st <st.stoyanov04@gmail.com>
2017-05-24 19:16:47 +0300

    social login

--- app/src/main/java/bg/uni_ruse/stoyanov/bsilentorganizer/MainActivity.java
+++ app/src/main/java/bg/uni_ruse/stoyanov/bsilentorganizer/MainActivity.java
@@ -4,16 +4,16 @@ import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.LayoutInflater;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
-import android.view.ViewGroup;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Toast;
 
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
+import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 import com.google.android.gms.auth.api.Auth;
@@ -27,7 +27,7 @@ import com.google.android.gms.common.api.GoogleApiClient;
 import com.facebook.FacebookSdk;
 import com.facebook.appevents.AppEventsLogger;
 
-import static bg.uni_ruse.stoyanov.bsilentorganizer.R.id.loginButton;
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity implements
         GoogleApiClient.OnConnectionFailedListener,
@@ -35,15 +35,51 @@ public class MainActivity extends AppCompatActivity implements
 
     private GoogleApiClient mGoogleApiClient;
     private final int RC_SIGN_IN = 9001;
-
-    CallbackManager callbackManager;
-
+    private CallbackManager mFacebookCallbackManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        AppEventsLogger.activateApp(this);
+
         setContentView(R.layout.activity_main);
-        findViewById(R.id.sign_in_button).setOnClickListener(this);
+
+        // Set the dimensions of the sign-in button.
+        SignInButton mGoogleSignInButton = (SignInButton) findViewById(R.id.google_sign_in_button);
+        mGoogleSignInButton.setSize(SignInButton.SIZE_WIDE);
+        mGoogleSignInButton.setColorScheme(SignInButton.COLOR_AUTO);
+        mGoogleSignInButton.setOnClickListener(this);
+
+        mFacebookCallbackManager = CallbackManager.Factory.create();
+        LoginButton mFacebookLoginButton = (LoginButton) findViewById(R.id.fb_login_button);
+        mFacebookLoginButton.setOnClickListener(this);
+        mFacebookLoginButton.registerCallback(mFacebookCallbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(final LoginResult loginResult) {
+                //TODO: Use the Profile class to get information about the current user.
+
+            }
+
+            @Override
+            public void onCancel() {
+
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                Log.d(MainActivity.class.getCanonicalName(), error.getMessage());
+
+            }
+        });
+    }
+
+    private void signInWithGoogle() {
+
+        if (mGoogleApiClient != null) {
+            mGoogleApiClient.disconnect();
+        }
 
         // Configure sign-in to request the user's ID, email address, and basic
         // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
@@ -58,21 +94,13 @@ public class MainActivity extends AppCompatActivity implements
                 .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
                 .build();
 
-        // Set the dimensions of the sign-in button.
-        SignInButton signInButton = (SignInButton) findViewById(R.id.sign_in_button);
-        signInButton.setSize(SignInButton.SIZE_WIDE);
-        signInButton.setColorScheme(SignInButton.COLOR_AUTO);
-
-
-
-
-        FacebookSdk.sdkInitialize(getApplicationContext());
-        AppEventsLogger.activateApp(this);
-        callbackManager = CallbackManager.Factory.create();
-       /* LoginButton loginButton = (LoginButton) view.findViewById(R.id.usersettings_fragment_login_button);
-        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() { ... });*/
+        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+        startActivityForResult(signInIntent, RC_SIGN_IN);
     }
 
+    private void signInWithFB() {
+        LoginManager.getInstance().logInWithReadPermissions(this, Arrays.asList("public_profile"));
+    }
 /*  Start login activity
     public void loginWithOrRegister(View view) {
         Intent intent = new Intent(this, loginWithOrRegister.class);
@@ -113,16 +141,13 @@ public class MainActivity extends AppCompatActivity implements
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
-            case R.id.sign_in_button:
-                signIn();
+            case R.id.google_sign_in_button:
+                signInWithGoogle();
                 break;
-            // ...
+            case R.id.fb_login_button:
+                signInWithFB();
+                break;
         }
-    }
-
-    private void signIn() {
-        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
-        startActivityForResult(signInIntent, RC_SIGN_IN);
     }
 
     @Override
@@ -132,58 +157,25 @@ public class MainActivity extends AppCompatActivity implements
         // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
         if (requestCode == RC_SIGN_IN) {
             GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
-            handleSignInResult(result);
-        }
+
+            if (result.isSuccess()) {
+                final GoogleApiClient client = mGoogleApiClient;
+                GoogleSignInAccount acct = result.getSignInAccount();
+                Context context = getApplicationContext();
+                CharSequence text = "Hello toast!";
+                int duration = Toast.LENGTH_SHORT;
+
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+            }
+            else {
+                Context context = getApplicationContext();
+                CharSequence text = "Bye toast!";
+                int duration = Toast.LENGTH_SHORT;
+
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+            }
+        } else mFacebookCallbackManager.onActivityResult(requestCode, resultCode, data);
     }
-
-    private void handleSignInResult(GoogleSignInResult result) {
-       // Log.d(TAG, "handleSignInResult:" + result.isSuccess());
-        if (result.isSuccess()) {
-            // Signed in successfully, show authenticated UI.
-            GoogleSignInAccount acct = result.getSignInAccount();
-            //mStatusTextView.setText(getString(R.string.signed_in_fmt, acct.getDisplayName()));
-            //updateUI(true);
-            Context context = getApplicationContext();
-            CharSequence text = "Hello toast!";
-            int duration = Toast.LENGTH_SHORT;
-
-            Toast toast = Toast.makeText(context, text, duration);
-            toast.show();
-        } else {
-            // Signed out, show unauthenticated UI.
-           // updateUI(false);
-        }
-    }
-/*
-    @Override
-    public View onCreateView(
-            LayoutInflater inflater,
-            ViewGroup container,
-            Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.splash, container, false);
-
-        loginButton = (LoginButton) view.findViewById(R.id.login_button);
-        loginButton.setReadPermissions("email");
-        // If using in a fragment
-        loginButton.setFragment(this);
-        // Other app specific specialization
-
-        // Callback registration
-        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
-            @Override
-            public void onSuccess(LoginResult loginResult) {
-                // App code
-            }
-
-            @Override
-            public void onCancel() {
-                // App code
-            }
-
-            @Override
-            public void onError(FacebookException exception) {
-                // App code
-            }
-        });
-    }*/
 }
