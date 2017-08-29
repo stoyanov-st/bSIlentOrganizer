ae0eaa023645cd65576cf8b0849f28cc83beed34
stoyanov-st <st.stoyanov04@gmail.com>
2017-08-29 13:11:57 +0300

    Flashlight module.

--- app/src/main/java/bg/uni_ruse/stoyanov/bsilentorganizer/FlashlightFragment.java
+++ app/src/main/java/bg/uni_ruse/stoyanov/bsilentorganizer/FlashlightFragment.java
@@ -1,18 +1,24 @@
 package bg.uni_ruse.stoyanov.bsilentorganizer;
 
+import android.*;
 import android.annotation.TargetApi;
 import android.content.Context;
-import android.hardware.camera2.CameraAccessException;
-import android.hardware.camera2.CameraManager;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.hardware.Camera;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CompoundButton;
+import android.widget.Switch;
 import android.widget.ToggleButton;
+
+import java.io.IOException;
 
 
 /**
@@ -23,12 +29,14 @@ import android.widget.ToggleButton;
  * Use the {@link FlashlightFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
+@SuppressWarnings("deprecation")
 public class FlashlightFragment extends Fragment {
 
-
-    private CameraManager cameraManager;
+    ToggleButton flashToggle;
+    private Camera camera;
+    private Camera.Parameters cameraParameters;
     private OnFragmentInteractionListener mListener;
-    private String cameraId;
+    private boolean backgroundUse = false;
 
     public FlashlightFragment() {
         // Required empty public constructor
@@ -38,9 +46,22 @@ public class FlashlightFragment extends Fragment {
         return new FlashlightFragment();
     }
 
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        checkForCameraPermissions();
+    }
+
+    @TargetApi(Build.VERSION_CODES.M)
+    private void checkForCameraPermissions() {
+        int hasCameraPermission = getActivity().checkSelfPermission(android.Manifest.permission.CAMERA);
+        if (hasCameraPermission != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(getActivity(),
+                    new String[]{android.Manifest.permission.CAMERA},
+                    1);
+        }
     }
 
     @Override
@@ -49,30 +70,47 @@ public class FlashlightFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_flashlight, container, false);
 
-        ToggleButton flashToggle = view.findViewById(R.id.flash_light_toggle);
-
+        flashToggle = view.findViewById(R.id.flash_light_toggle);
+        Switch backgroundModeSwitch = view.findViewById(R.id.use_in_bg_switch);
 
         flashToggle.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
 
             @Override
             public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
                 if (b) {
-                    try {
-                        cameraManager.setTorchMode(cameraId, true);
-                    } catch (CameraAccessException e) {
-                        e.printStackTrace();
-                    }
+                    new Thread(new Runnable() {
+                        @Override
+                        public void run() {
+                            turnFlashOn();
+                        }
+                    }).start();
                 }
                 else {
-                    try {
-                        cameraManager.setTorchMode(cameraId, false);
-                    } catch (CameraAccessException e) {
-                        e.printStackTrace();
-                    }
+                        new Thread(new Runnable() {
+                            @Override
+                            public void run() {
+                                try {
+                                    turnFlashOff();
+                                } catch (IOException e) {
+                                    camera.release();
+                                    camera = Camera.open();
+                                    e.printStackTrace();
+                                }
+                            }
+                        }).start();
                 }
             }
         });
 
+
+        backgroundModeSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                backgroundUse = b;
+            }
+        });
+
+        backgroundModeSwitch.setChecked(getSavedBackgroundMode());
 
         return view;
     }
@@ -99,6 +137,57 @@ public class FlashlightFragment extends Fragment {
     public void onDetach() {
         super.onDetach();
         mListener = null;
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        if (!backgroundUse && camera != null) {
+            flashToggle = getActivity().findViewById(R.id.flash_light_toggle);
+            flashToggle.setChecked(false);
+        }
+
+
+        SharedPreferences sharedPreferences = getActivity().getApplicationContext()
+                .getSharedPreferences("flashLightPrefs", Context.MODE_PRIVATE);
+
+        sharedPreferences.edit()
+                .putBoolean("backgroundMode", backgroundUse)
+                .apply();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+
+        camera.release();
+    }
+
+    private void turnFlashOn(){
+        camera = Camera.open();
+        cameraParameters = camera.getParameters();
+        cameraParameters.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
+        camera.setParameters(cameraParameters);
+        camera.startPreview();
+
+    }
+
+    private void turnFlashOff() throws IOException {
+
+        camera.reconnect();
+        cameraParameters = camera.getParameters();
+        cameraParameters.setFlashMode(Camera.Parameters.FLASH_MODE_OFF);
+        camera.setParameters(cameraParameters);
+        camera.stopPreview();
+        camera.release();
+    }
+
+    private boolean getSavedBackgroundMode() {
+        SharedPreferences sharedPreferences = getActivity().getApplicationContext()
+                .getSharedPreferences("flashLightPrefs", Context.MODE_PRIVATE);
+
+        return sharedPreferences.getBoolean("backgroundMode", false);
     }
 
     /**
