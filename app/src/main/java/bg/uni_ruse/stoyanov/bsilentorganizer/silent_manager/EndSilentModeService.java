e242d867668fb28beeb32425b4adf15e0e6863f3
S.Stoyanov <stoyanov-st@users.noreply.github.com>
2017-12-05 11:22:55 +0200

    Update EndSilentModeService.java

--- app/src/main/java/bg/uni_ruse/stoyanov/bsilentorganizer/silent_manager/EndSilentModeService.java
+++ app/src/main/java/bg/uni_ruse/stoyanov/bsilentorganizer/silent_manager/EndSilentModeService.java
@@ -12,20 +12,32 @@ import android.widget.Toast;
  * Created by stoyanovst on 9/7/17.
  */
 
-public class EndSilentModeService extends Service {
+public class EndSilentModeService extends Service implements RingMode {
 
     static int SERVICE_ID = 420;
-    private AudioManager audioManager;
 
+    @Override
+    public boolean setRingerMode() {
+        try {
+            AudioManger audioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+            audioManager.setRingerMode(AudioManager.RINGER_MODE_NORMAL);
+            return true;
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+    
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
 
-        audioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
-        audioManager.setRingerMode(AudioManager.RINGER_MODE_NORMAL);
+        if (setRingerMode()) {
 
         Toast.makeText(getApplicationContext(), "Silent Mode End", Toast.LENGTH_SHORT).show();
 
         return START_STICKY;
+        } else return false;
     }
 
     @Nullable
